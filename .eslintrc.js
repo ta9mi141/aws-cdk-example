fdb5715cef65dd1b13298d62d5c4f8ddb62a4d44
it_akumi <it.akumi1011@gmail.com>
2019-08-22 08:55:24 +0900

    Add eslint and prettier to devDependencies

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        sourceType: "module"
+    },
+    rules: {}
+};
