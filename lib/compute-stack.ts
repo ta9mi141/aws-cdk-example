612fe63fea91927e16f6191089a1a19ef32a1b71
it_akumi <it.akumi1011@gmail.com>
2019-08-23 17:44:47 +0900

    Remove an ALB from ComputeStack

--- lib/compute-stack.ts
+++ lib/compute-stack.ts
@@ -1,6 +1,5 @@
 import cdk = require("@aws-cdk/core");
 import ec2 = require("@aws-cdk/aws-ec2");
-import elbv2 = require("@aws-cdk/aws-elasticloadbalancingv2");
 
 interface ComputeStackProps extends cdk.StackProps {
     vpc: ec2.Vpc;
@@ -11,35 +10,27 @@ export class ComputeStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props: ComputeStackProps) {
         super(scope, id, props);
 
-        const alb = new elbv2.ApplicationLoadBalancer(this, "Alb", {
-            vpc: props.vpc,
-            internetFacing: true,
-            loadBalancerName: "Example-Alb"
-        });
-        const listener = alb.addListener("Listener", {
-            port: 80,
-            open: true
-        });
-
         const userData = ec2.UserData.forLinux({ shebang: "#!/bin/bash" });
-        userData.addCommands("yum -y update", "yum -y install nginx", "systemctl enable nginx", "systemctl start nginx");
+        userData.addCommands("amazon-linux-extras -y install nginx1.12", "systemctl enable nginx", "systemctl start nginx");
         for (let privateSubnet of props.vpc.privateSubnets) {
-            const instance = new ec2.CfnInstance(this, `WebInstance-${privateSubnet.node.id}`, {
-                instanceType: ec2.InstanceSize.NANO,
+            new ec2.CfnInstance(this, `WebInstance-${privateSubnet.node.id}`, {
+                imageId: new ec2.AmazonLinuxImage({
+                    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+                    userData: userData
+                }).getImage(this).imageId,
+                instanceType: ec2.InstanceType.of(
+                    ec2.InstanceClass.T3,
+                    this.node.tryGetContext("env") === "production": ec2.InstanceSize.MEDIUM: ec2.InstanceSize.NANO
+                ).toString(),
                 keyName: this.node.tryGetContext("key"),
                 subnetId: privateSubnet.subnetId,
                 securityGroupIds: [props.internalSG.securityGroupId],
                 tags: [
                     {
                         key: "Name",
-                        value: "Example-Web-${privateSubnet.availabilityZone}"
+                        value: `Example-Web-${privateSubnet.availabilityZone}`
                     }
-                ],
-                userData: cdk.Fn.base64(userData.render())
-            });
-            listener.addTargets(`Target-${privateSubnet.node.id}`, {
-                port: 80,
-                targets: [new elbv2.InstanceTarget(instance.ref.toString())]
+                ]
             });
         }
     }
