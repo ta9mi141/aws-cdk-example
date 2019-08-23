34caafffae569162abb03aa1d33b5c2f1afb7869
it_akumi <it.akumi1011@gmail.com>
2019-08-23 17:47:19 +0900

    Make sure to create EC2 instances with Nginx

--- lib/compute-stack.ts
+++ lib/compute-stack.ts
@@ -11,16 +11,17 @@ export class ComputeStack extends cdk.Stack {
         super(scope, id, props);
 
         const userData = ec2.UserData.forLinux({ shebang: "#!/bin/bash" });
-        userData.addCommands("amazon-linux-extras -y install nginx1.12", "systemctl enable nginx", "systemctl start nginx");
+        userData.addCommands(
+            "amazon-linux-extras install -y nginx1.12",
+            "systemctl enable nginx",
+            "systemctl start nginx"
+        );
         for (let privateSubnet of props.vpc.privateSubnets) {
             new ec2.CfnInstance(this, `WebInstance-${privateSubnet.node.id}`, {
-                imageId: new ec2.AmazonLinuxImage({
-                    generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-                    userData: userData
-                }).getImage(this).imageId,
+                imageId: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }).getImage(this).imageId,
                 instanceType: ec2.InstanceType.of(
                     ec2.InstanceClass.T3,
-                    this.node.tryGetContext("env") === "production": ec2.InstanceSize.MEDIUM: ec2.InstanceSize.NANO
+                    this.node.tryGetContext("env") === "production" ? ec2.InstanceSize.MEDIUM : ec2.InstanceSize.NANO
                 ).toString(),
                 keyName: this.node.tryGetContext("key"),
                 subnetId: privateSubnet.subnetId,
@@ -30,7 +31,8 @@ export class ComputeStack extends cdk.Stack {
                         key: "Name",
                         value: `Example-Web-${privateSubnet.availabilityZone}`
                     }
-                ]
+                ],
+                userData: cdk.Fn.base64(userData.render())
             });
         }
     }
